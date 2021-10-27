049b24fccd53476e742584cf846c273b02f60b29
INviZEo <lev19825@mail.ru>
2021-10-27 22:59:51 +0300

    0. Что такое переменная. [#3610]

---
+++ src/main/java/Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+
+    private static int mem;
+
+    public static void main(String[] args) {
+          int age = 18;
+          System.out.println(age);
+          float length = (float) 78.0332;
+          System.out.println(length);
+          boolean baby = true;
+          System.out.println(baby);
+      }
+}
