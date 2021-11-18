23d35b0d9a2b1a1d59d37e1bb015eae442f1294d
INviZEo <lev19825@mail.ru>
2021-11-19 00:14:02 +0300

    4.3.3. Логическое отрицание ! [#334072 #229456]

--- src/main/java/ru/job4j/condition/Triangle.java
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -2,9 +2,6 @@ package ru.job4j.condition;
 
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
-        boolean a = (ab + ac) > bc;
-        boolean b = (ac + bc) > ab;
-        boolean c = (ab + bc) > ac;
-        return a && b && c;
+        return ((ab + ac) > bc) && ((ab + ac) > bc) && ((ab + bc) > ac);
     }
 }
