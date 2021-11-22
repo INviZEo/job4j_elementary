7918a2888b855debb8c9707241f6e6fe22e18b6d
INviZEo <lev19825@mail.ru>
2021-11-23 02:21:24 +0300

    5.0.2. Вечный цикл for. [#226417]

--- src/main/java/ru/job4j/loop/InfinityLoop.java
+++ src/main/java/ru/job4j/loop/InfinityLoop.java
@@ -4,7 +4,8 @@ public class InfinityLoop {
     public static void main(String[] args) {
         for (int index = 0; index < 10; index++) {
             System.out.println("infinity");
-            index = index - 1;
+            index = index - 1; /*Эта строка вызывает бесконечный цикл, потому что условие цикла будет всегда верным,
+             переменная index будет принимать значение 0 либо -1 и не дойдёт до 10*/
         }
     }
 }
