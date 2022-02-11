5724c0b2070498857166a95fdeb6694f3c3db1ad
INviZEo <lev19825@mail.ru>
2022-02-11 20:08:36 +0300

    Check

---
+++ src/main/java/ru/job4j/calculator/OPG.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculator;
+
+public class OPG {
+    public static void out(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+}
