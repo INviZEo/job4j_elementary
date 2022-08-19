3e99d3a24ecb500f117fb90d383c8dbd8f846381
INviZEo <lev19825@mail.ru>
2022-08-20 00:11:51 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean rsl = false;
+        if (name.isEmpty()) {
+            rsl = false;
+        }
+
+        for (int i = 0; i < name.length(); i++) {
+            if (isSpecialSymbol(name.codePointAt(i)) || Character.isUpperCase(name.codePointAt(i))
+                    || isUpperLatinLetter(name.codePointAt(i))
+                    || isLowerLatinLetter(name.codePointAt(i))) {
+                rsl = true;
+            }
+        }
+        for (int i = 0; i < name.length(); i++) {
+            if (Character.isUpperCase(name.codePointAt(0))) {
+                rsl = false;
+            }
+        }
+
+        for (int i = 0; i < name.length(); i++) {
+            if (Character.isDigit(name.codePointAt(0))) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 || code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 || code <= 122;
+    }
+}
