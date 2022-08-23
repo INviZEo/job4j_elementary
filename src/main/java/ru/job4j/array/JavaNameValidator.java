43832f078e19b5f67c6b5af741971e783540e94c
INviZEo <lev19825@mail.ru>
2022-08-23 23:56:55 +0300

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,19 +2,23 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean rsl = false;
-        if (name.isEmpty() || Character.isUpperCase(name.codePointAt(0))
-                || Character.isDigit(name.codePointAt(0))) {
-            return rsl;
-        }
-        for (int i = 1; i < name.length(); i++) {
-            if (isSpecialSymbol(name.codePointAt(i)) || Character.isUpperCase(name.codePointAt(i))
-                    || isUpperLatinLetter(name.codePointAt(i))
-                    || isLowerLatinLetter(name.codePointAt(i))) {
-                rsl = true;
+        boolean rsl;
+        rsl = name.isEmpty()
+                || Character.isUpperCase(name.codePointAt(0))
+                || Character.isDigit(name.codePointAt(0));
+        if (!rsl) {
+            for (int i = 1; i < name.length(); i++) {
+                if (!isSpecialSymbol(name.codePointAt(i))
+                        || !Character.isUpperCase(name.codePointAt(i))
+                        || !isUpperLatinLetter(name.codePointAt(i))
+                        || !isLowerLatinLetter(name.codePointAt(i))
+                        || !Character.isDigit(name.codePointAt(i))) {
+                    rsl = false;
+                    break;
+                }
             }
         }
-        return rsl;
+        return !rsl;
     }
 
     public static boolean isSpecialSymbol(int code) {
@@ -22,10 +26,10 @@ public class JavaNameValidator {
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return code >= 65 || code <= 90;
+        return code >= 65 && code <= 90;
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return code >= 97 || code <= 122;
+        return code >= 97 && code <= 122;
     }
 }
